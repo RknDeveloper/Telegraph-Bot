d89943202b5e35deb61c06a92aa0b579b13a009d
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2024-03-01 07:37:52 +0545

    @Rkn_TelegraphBot

---
+++ helper/database.py
@@ -0,0 +1,41 @@
+import motor.motor_asyncio
+from config import Config
+from .utils import send_log
+
+class Database:
+
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.user
+
+    def new_user(self, id):
+        return dict(
+            _id=int(id))                               
+            
+    async def add_user(self, b, m):
+        u = m.from_user
+        if not await self.is_user_exist(u.id):
+            user = self.new_user(u.id)
+            await self.col.insert_one(user)            
+            await send_log(b, u)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'_id': int(user_id)})    
+
+    
+db = Database(Config.DB_URL, Config.DB_NAME)
+
+
---
+++ helper/utils.py
@@ -0,0 +1,17 @@
+import time
+from datetime import datetime
+from pytz import timezone
+from config import Config 
+
+
+async def send_log(b, u):
+    if Config.LOG_CHANNEL is not None:
+        curr = datetime.now(timezone("Asia/Kolkata"))
+        date = curr.strftime('%d %B, %Y')
+        time = curr.strftime('%I:%M:%S %p')
+        await b.send_message(
+            Config.LOG_CHANNEL,
+            f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: {b.mention}"
+        )
+        
+
